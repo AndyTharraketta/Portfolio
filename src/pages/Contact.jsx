4b1bb66bc2a0d5bba258618005e86a90c104225c
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2025-06-03 19:44:01 +0200

    edit Contact.jsx adding links to my GitHub

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React from "react";
+import "../styles/Contact.css";
 
 function Contact() {
   return (
-    <div>
+    <div className="contact-container">
       <h1>Kontakt</h1>
-      <p>Kontaktiere uns über das folgende Formular oder per E-Mail.</p>
+      <p>Hier findet Ihr die Links zu meinem GitHub</p>
+      <p>
+        Link GitHub:&nbsp;
+        <a
+          href="https://github.com/AndyTharraketta"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-link"
+        >
+          AndyTharraketta
+        </a>
+      </p>
+      <p>
+        Link GitHub-Projekt:&nbsp;
+        <a
+          href="https://github.com/AndyTharraketta/Portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-link"
+        >
+          Portfolio-Projekt
+        </a>
+      </p>
     </div>
   );
 }
-
 export default Contact;
